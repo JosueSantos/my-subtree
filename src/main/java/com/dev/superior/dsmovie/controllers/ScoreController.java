1c95647a54e605783255e73ec8f6be321471f688
Josue Dos Santos Marques <josue.santos@verdesmares.com.br>
2022-03-13 22:10:08 -0300

    Save, useNavigate

--- src/main/java/com/dev/superior/dsmovie/controllers/ScoreController.java
+++ src/main/java/com/dev/superior/dsmovie/controllers/ScoreController.java
@@ -5,6 +5,7 @@ import com.dev.superior.dsmovie.dto.ScoreDTO;
 import com.dev.superior.dsmovie.services.ScoreService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class ScoreController {
     @Autowired
     private ScoreService service;
     
+    @PutMapping
     public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
         MovieDTO movieDTO = service.saveScore(dto);
         return movieDTO;
